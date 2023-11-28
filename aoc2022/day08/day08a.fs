5147e05072ef3a3a7cef879624bd913661e08447
Ian Simpson <ian.simpson.nz@gmail.com>
2023-11-28 14:34:10 +1300

    Day08a - read file & build tree array

--- aoc2022/day08/day08a.fs
+++ aoc2022/day08/day08a.fs
@@ -3,6 +3,31 @@ require ~/forth/libs/files.fs
 128 Constant max-line
 Create line-buffer  max-line 2 + allot
 
+variable #rows
+variable #cols
+variable 'forrest
+variable 'cansee
+
+: at                ( 'array col row -- byte-addr)   \ Usage 'forrest 5 10 at @c
+    #cols * + swap @ +
+;
+
+: .array            ( 'array -- )
+    @                                   ( addr )
+    #rows @ #cols @ * 0 do                  ( addr )
+        i #cols @ mod 0= if cr then
+        dup i + c@ .
+    loop drop cr
+;
+
+: my-cmove          ( from to #chars -- )
+    0 do                            ( from to )
+        swap dup 1+ swap            ( to from+1 from)
+        c@ [char] 0 -               ( to from+1 c-0 )
+        rot dup 1+ rot rot c!       ( from+1 to+1 )
+    loop 2drop
+;
+
 : test     ( -- file-name-addr file-name-len )
     s" test.txt" ;
 
@@ -16,12 +41,39 @@ Create line-buffer  max-line 2 + allot
 : get-line      ( buff -- n-chr flag )
     line-buffer max-line fd-in read-line throw ;
 
+: check-line-len    ( #cols len -- #cols )
+    2dup <> if                  ( #c len )           \ If line len <> num columns
+        swap dup 0<> if          ( len #c )           \ OK if this is the first line, set num cols
+            -1 abort" Invalid line length" then        \ Otherwise mismatching line lengths
+    then drop                   ( #c )
+;
 
-: process
+: create-arrays     ( -- )
+    0 0                                     ( #rows #cols )
+    here 'forrest !
+    begin get-line while                    ( #r #c len )
+        check-line-len                      ( #r #c )
+        line-buffer over here swap          ( #r #c lb here #c )
+        dup allot                           ( #r #c lb here #c )
+        my-cmove                            ( #r #c )
+        swap 1+ swap                        ( #r+1 #c )
+    repeat drop                             ( #r #c )
+    here 'cansee ! 2dup * allot             ( #r #c )
+    #cols ! #rows !
+;
+
+: process       ( -- )
+\    0 #rows !
+\    0 #cols !
+    create-arrays
+    cr ." r c = " #rows ? #cols ? cr
+    ." forrest addr = " 'forrest ? cr
+    .s cr
+    'forrest .array
 ;
 
 : .result       ( -- )
-    ." The answer is: " . cr
+    ." The answer is: " cr
 ;
 
 : go
