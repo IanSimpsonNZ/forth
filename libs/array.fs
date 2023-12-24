086260ca2f908c434b456539be03c161fedb9f8e
Ian Simpson <ian.simpson.nz@gmail.com>
2023-12-24 18:20:17 +1300

    Day 11 Part 1

--- libs/array.fs
+++ libs/array.fs
@@ -1,2 +1,5 @@
 : array ( num-elements -- ) create cells allot
     does> ( element-number -- addr ) swap cells + ;
+
+: b-array ( num-elements -- ) create dup , cells allot ;
+(    does> ( element-number -- addr  2dup @ < if swap cells + 1+ else -1 abort" b-array range overflow" then ; )
