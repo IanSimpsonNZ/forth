98c259e77ee2b7c8182a8a77bc03801e9dff1d3d
Ian Simpson <ian.simpson.nz@gmail.com>
2023-12-25 22:53:40 +1300

    Add array type with bound checks to lib

--- libs/array.fs
+++ libs/array.fs
@@ -1,5 +1,6 @@
-: array ( num-elements -- ) create cells allot
+: array ( num-elements -- ) create cells allot                      \ Simple array - usage "100 array myarray", then "el-num myarray @"
     does> ( element-number -- addr ) swap cells + ;
 
-: b-array ( num-elements -- ) create dup , cells allot ;
-(    does> ( element-number -- addr  2dup @ < if swap cells + 1+ else -1 abort" b-array range overflow" then ; )
+: b-array ( num-elements -- ) create dup , cells allot              \ Array with bound checks - usage "100 b-array myarray2", then "el-num myarray2 @"
+    does> ( element-number -- addr ) 2dup @ over 0>= rot rot < and if swap 1+ cells + else -1 abort" b-array range overflow" then ;
+
